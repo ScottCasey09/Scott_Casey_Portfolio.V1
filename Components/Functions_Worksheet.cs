e6aecc1d92b0e4879b9fec448cd1d64b43c3e2d9
casey scott <Cjscott@yahoo.com>
2016-01-10 20:31:19 -0500

    New Version 1.2
    Added more files to all folders.

---
+++ Components/Functions_Worksheet.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Functions_Worksheet
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			/*
+			Casey Scott
+			11-10-2015
+			section 01
+			Functions worksheet
+			*/
+
+			//Write out our givens
+
+			decimal originalCost = 20m;
+			decimal taxPercent = .07m;
+
+			//Go write our finction
+			//function call
+			//Remember to create a variable to catch the returned value
+			decimal results = totalCost(originalCost, taxPercent);
+
+			//Report results
+			Console.WriteLine("The total cost of the item plus tax is ${0}.\r\n",results);
+
+			//End of Plus Tax - all values checked
+
+
+
+			//Givens for the circumference of a circle
+			double radius = 4;
+			double pie = Math.PI;
+			double circResults = circumferenceOfACircle(radius, pie);
+
+
+			//Report the result of the circumference
+		Console.WriteLine("The circumference of a circle with the radius of "+radius+", is {0}.\r\n ", circResults);
+
+		
+
+			//Givens for stings
+			double victimsWeightlb = 200;
+			double stingTakes = 8.666666667;
+			double stingResults = beeStings (stingTakes,victimsWeightlb);
+
+			//Print results of bee stings
+			Console.WriteLine("It takes {0} bee stings to kill this animal.",stingResults);
+
+		}
+		//Sample problem
+		public static decimal totalCost(decimal cost, decimal tax){
+
+			//Do math in order to find total cost
+			decimal taxMoney = cost * tax;
+
+			//Add tax money to the actual cost
+			decimal totalCost = cost + taxMoney;
+
+				//Return the value back to our main method
+				return totalCost;
+
+
+		}
+		//Circumference of a circle
+	
+		public static double circumferenceOfACircle(double rad,double pi)
+		{
+			//Do the math for the circumference of a circle
+			double circumferenceOfACircle = rad * pi;
+
+			//return this to the main
+			return circumferenceOfACircle;
+
+	
+		}
+
+
+		//Stung!
+		public static double beeStings(double stings,double weightlb)
+		{
+			//Do the math
+			double beeStings = weightlb/stings;
+
+			//Return to main
+			return beeStings;
+
+
+
+
+		}
+
+	}
+
+}
---
+++ Components/Zombie_Maddness.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ZombieLoopingMadness
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Zombie Madness
+
+			//Givens first
+			//Start with 1 zombie
+			//It can bite 4 people in a day
+			//8 days.
+
+			//How many zombies do we have?
+			int numZombies = 1;
+
+			//Number of bites per zombie per day
+			int numBites = 4;
+
+			//How many days?
+			int days = 8;
+
+			//Create our for to cycle through each day
+			for(int i =1; i<=days; i++){
+
+				//What happens in a day?
+				//How many new zombies get created?
+				int newZombies = numZombies * numBites;
+
+				//end of the day these new zombies join our zombie horde
+				numZombies += newZombies;
+
+				//Tell the public how many zombies we have each day
+				Console.WriteLine("There are {0} zombieson day #{1}",numZombies,i);
+
+
+				
+			}
+
+			//How lon it will take to reach a million zombies
+
+			int numDays = 1;
+
+			int ZombieHoardNumber = 1;
+
+			while(ZombieHoardNumber<=1000000){
+
+				//Happens each day
+				int bittenPeople = ZombieHoardNumber * numBites;
+
+				//end of the day those become zombies
+
+				ZombieHoardNumber += bittenPeople;
+
+				//Report to the people how many zombies there are
+				Console.WriteLine("On day #{0}, there are {1} zombies!", numDays, ZombieHoardNumber);
+
+				//End of the day, we increase the day #
+				numDays++;
+				
+			}
+		}
+	}
+}
